83692bd834dfae451f9e496906406f4a6e2735a5
slavik-gassiev <slavikgassiev1872002@gmail.com>
2025-05-13 14:36:07 +0300

    Added kafka sending function. Created KafkaTopicConfig, KafkaProducerConfig and sendMessages in KafkaService

--- task-tracker-scheduler/src/main/java/cam/slava/learn/controller/SchedulerController.java
+++ task-tracker-scheduler/src/main/java/cam/slava/learn/controller/SchedulerController.java
@@ -2,8 +2,10 @@ package cam.slava.learn.controller;
 
 import cam.slava.learn.dto.MessageDto;
 import cam.slava.learn.dto.UserDto;
+import cam.slava.learn.service.KafkaService;
 import cam.slava.learn.service.MessageService;
 import cam.slava.learn.service.UserService;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -14,16 +16,20 @@ import java.util.List;
 public class SchedulerController {
     private final UserService userService;
     private final MessageService messageService;
+    private final KafkaService kafkaService;
 
-    public SchedulerController(UserService userService, MessageService messageService) {
+    public SchedulerController(UserService userService, MessageService messageService, KafkaService kafkaService) {
         this.userService = userService;
         this.messageService = messageService;
+        this.kafkaService = kafkaService;
     }
 
-    @Scheduled()
+    @Scheduled(cron = "0 0 18 * * *")
     public void start() {
         List<UserDto> allUsers = userService.getAllUsers();
         List<MessageDto> messages = messageService.createMessages(allUsers);
+        kafkaService.sendMessages(messages);
+
 
     }
 }
--- task-tracker-scheduler/src/main/java/cam/slava/learn/service/KafkaService.java
+++ task-tracker-scheduler/src/main/java/cam/slava/learn/service/KafkaService.java
@@ -1,4 +1,26 @@
 package cam.slava.learn.service;
 
+import cam.slava.learn.dto.MessageDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class KafkaService {
+    private final KafkaTemplate<String, MessageDto> kafkaTemplate;
+
+    @Value(value = "${spring.kafka.topic}")
+    private String topic;
+
+    public KafkaService(KafkaTemplate<String, MessageDto> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessages(List<MessageDto> messages) {
+        for (MessageDto messageDto :    messages) {
+            kafkaTemplate.send(topic, messageDto);
+        }
+    }
 }
